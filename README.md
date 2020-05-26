41e9e2cf3b5b84ddb3bb4e49be3ccdb03a746de4
shubhayan saha <shubhayan_saha@srmuniv.edu.in>
2020-05-27 00:31:09 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,52 @@
+# eLab Report Downloader
+
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://github.com/ShubhayanS/eLab-Reports-Downloader/graphs/commit-activity) 
+[![GitHub issues](https://img.shields.io/github/issues/ShubhayanS/eLab-Reports-Downloader)](https://github.com/ShubhayanS/eLab-Reports-Downloader/issues)
+[![GitHub forks](https://img.shields.io/github/forks/ShubhayanS/eLab-Reports-Downloader?style=social)](https://github.com/ShubhayanS/eLab-Reports-Downloader/network) [![GitHub stars](https://img.shields.io/github/stars/ShubhayanS/eLab-Reports-Downloader?style=social)](https://github.com/ShubhayanS/eLab-Reports-Downloader/stargazers)
+ [![Open Source Love svg1](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)](https://github.com/ellerbrock/open-source-badges/)
+
+
+## What it does?
+
+This script is for downloading reports of eLab automatically thus saving the hassle of individually downloading the reports.
+
+```3rd year``` : OS (C, C++)
+
+## :rocket: Getting Started 
+
+### Disclaimer
+
+Run this script at your own risk. The developers are no way responsible for unforseen issues with  your eLab. However, this script is tested extensively to avoid such circumstances.
+
+## :octocat: Requirements
+
+1. [Python 3](https://www.python.org/downloads/) installed on your system.
+2. Python Selenium (It can be installed by opening CMD and running "pip install selenium")
+3. Urllib Library (It can be installed by opening CMD and running "pip install urllib")
+4. Latest version of Google Chrome (>83.0) installed.
+5. Good Internet Connection - Any issues after running the script can be resolved by inproving your connection.
+
+
+### :running: Running
+
+1. Clone the repository, and navigate to the downloaded folder `eLab-Reports-Downloader`.
+2. Open CMD where the folder is located and type ```python elab.py``` .
+3. You will be prompted for Registration Number and Password. Type them one by one and press Enter.
+4. Your eLab reports will automatically get downloaded and saved in Downloads (default chrome download location) in your PC.
+
+## :stars: Contribute
+
+This project repository was made for personal use. Feel free to fork the repository and make changes corresponding to your eLab to improve support. Some pending work can be :
+
+- [ ] Support for multiple elabs (example - ADA, Python etc)
+- [ ] Package the web application also as a native app/ extension/ web-app.
+- [ ] Cloud-hosted with full support on Heroku / Ripple / AWS
+- [ ] Click-and-go : Allow user to be notified and PDF emailed when report generation is complete
+
+### Current Maintainers
+
+[ShubhayanS](https://github.com/ShubhayanS), [Soumya Sinha](https://github.com/harshithmohan), [Sagnik Chatterjee](https://github.com/catthatcodes)
+
+
+That's it. :smiley:
+
---
+++ elab.py
@@ -0,0 +1,96 @@
+from selenium import webdriver
+
+from urllib.parse import quote   
+
+import re
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from time import sleep
+
+url = 'https://care.srmist.edu.in/srmos/login'
+driver = webdriver.Chrome()
+driver.get(url)
+driver.implicitly_wait(60)
+
+#print("Enter your registration number (case-sensitive)")
+user = input("Enter your registration number (case-sensitive)")
+#user = "RA1711003011375"
+
+#print("Enter your password (case-sensitive)")
+pw = input("Enter your password (case-sensitive)")
+#pw = "soumy@29"
+
+s=".mat-input-element"
+obj= driver.find_elements_by_css_selector(s)
+print(obj)
+c=0
+for i in obj:
+    #print(i)
+    if(c==0):
+        i.send_keys(user)
+        c=1
+    else:
+        i.send_keys(pw)
+
+btn = driver.find_element_by_xpath("/html/body/app-root/div/app-login/div/mat-card/div[2]/form/button")
+
+        
+btn.click()
+
+status = driver.find_element_by_xpath("/html/body/app-root/div/app-student-home/div/mat-card/div/div/app-student-home-card/mat-card")
+status.click()
+
+qp = driver.find_element_by_css_selector('#svgChart > g > g:nth-child(4) > path:nth-child(1)')
+
+qp.click()
+
+prev = driver.find_element_by_xpath("/html/body/app-root/div/app-student-solve/div[1]/button[1]") 
+
+for i in range(100):
+    sleep(10)
+    print("I am in question ",i)
+    sleep(15)
+    try:
+        evaluate = driver.find_element_by_xpath("/html/body/app-root/div/app-student-solve/div[2]/app-solve-question/div/div[2]/div[2]/mat-card/div[3]/button[2]")
+        evaluate.click()
+        result = driver.find_elements_by_xpath("/html/body/app-root/div/app-student-solve/div[2]/app-solve-question/div/div[2]/div[2]/mat-card/div[4]/a[1]")
+        txt = result[0].text        
+        print("C ",txt,"@")
+        sleep(5)
+        if(txt == 'RESULT - 100%'):
+            report = driver.find_element_by_xpath("/html/body/app-root/div/app-student-solve/div[2]/app-solve-question/div/div[2]/div[2]/mat-card/div[4]/a[2]")
+            report.click()
+            sleep(10)
+            prev.click()
+        else:
+            trigger = driver.find_element_by_xpath("/html/body/app-root/div/app-student-solve/div[2]/app-solve-question/div/div[1]/mat-form-field/div/div[1]/div/mat-select/div")
+            trigger.click()
+            cpp = driver.find_elements_by_css_selector(".mat-select-panel .mat-optgroup-label, .mat-select-panel .mat-option")
+            opt = 0
+            for j in cpp:
+                if(opt!=0):
+                    j.click()
+                else:
+                    opt = 1
+            sleep(10)
+            evaluate = driver.find_element_by_xpath("/html/body/app-root/div/app-student-solve/div[2]/app-solve-question/div/div[2]/div[2]/mat-card/div[3]/button[2]")
+            evaluate.click()
+            result = driver.find_elements_by_xpath("/html/body/app-root/div/app-student-solve/div[2]/app-solve-question/div/div[2]/div[2]/mat-card/div[4]/a[1]")
+            txt = result[0].text
+            print("CPP ", txt)
+            if(txt == 'RESULT - 100% '):
+                report = driver.find_element_by_xpath("/html/body/app-root/div/app-student-solve/div[2]/app-solve-question/div/div[2]/div[2]/mat-card/div[4]/a[2]")
+                report.click()
+                sleep(10)
+                prev.click()
+                break
+            else:
+                print("QP not evaluated fully")
+                prev.click()
+    except:
+        print("Unknown Error")
+        sleep(5)
+        prev.click()
